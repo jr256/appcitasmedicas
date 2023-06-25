597560310b95d9c90f0db68ae832af40fab3198f
jr256 <joser.72@outlook.com>
2023-06-24 19:06:25 -0500

    layout

--- src/main/java/pe/edu/cibertec/appcitasmedicas/controller/HomeController.java
+++ src/main/java/pe/edu/cibertec/appcitasmedicas/controller/HomeController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-	@GetMapping("/home")
-	public String home() {
-		return "home";
-	}
+
 	
 	@GetMapping("/homePaciente")
 	public String homepaciente() {
